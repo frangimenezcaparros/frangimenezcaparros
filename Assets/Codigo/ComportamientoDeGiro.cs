c1127e146b4544e261bfdd99554f4baebcdc32a9
frangimenezcaparros <frangimenezcaparros@gmail.com>
2019-12-13 08:40:43 +0100

    13/12/2019
    Todas las escenas

---
+++ Assets/Codigo/ComportamientoDeGiro.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComportamientoDeGiro : MonoBehaviour
+{
+    Vector3 velocidadRotacion = new Vector3(15, 30, 45);
+
+    void Update()
+    {
+        transform.Rotate(velocidadRotacion * Time.deltaTime);
+    }
+}
---
+++ Assets/Codigo/ControladorDelJugador.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControladorDelJugador : MonoBehaviour
+{
+    //Almacena componente Rigidbody del personaje Jugador.
+    Rigidbody rb;
+    public float velocidad;
+    public int contador;
+    public Text marcador;
+    public Text findejuego;
+
+    public void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        contador = 0;
+        ActualizarMarcador();
+        findejuego.gameObject.SetActive(false);
+    }
+    public void fixedUpdate()
+    {
+        float movimientoHorizontal = Input.GetAxis("Horizontal");
+        float movimientoVertical = Input.GetAxis("Vertical");
+
+        Vector3 movimiento = new Vector3(movimientoHorizontal, 0f, movimientoVertical);
+        rb.AddForce(movimiento * velocidad);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        contador = contador + 1;
+        ActualizarMarcador();
+        if (contador >= 5)
+        {
+            findejuego.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    void ActualizarMarcador()
+    {
+        marcador.text = "Puntos: " + contador;
+    }
+}     
